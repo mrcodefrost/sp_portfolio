41b5888f50ad9c1ab6b78d301d8476e878170073
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2026-04-10 16:37:23 +0530

    Update : Dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  serverExternalPackages: ["@sanity/client", "sanity"],
+  experimental: {
+    optimizePackageImports: ["react-icons", "@heroicons/react"],
+  },
+};
 
 export default nextConfig;
